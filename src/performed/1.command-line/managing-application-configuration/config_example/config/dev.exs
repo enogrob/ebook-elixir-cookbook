1e731ab35ea335c76b50ab10ff1e3c709ea23901
Roberto Nogueira <enogrob@gmail.com>
2019-10-16 08:41:49 -0300

    performed-chapter-1-managing-application-configuration

---
+++ src/performed/1.command-line/managing-application-configuration/config_example/config/dev.exs
@@ -0,0 +1,5 @@
+
+use Mix.Config
+
+config :config_example,
+  message_two: "I'm a development environment message!"
---
+++ src/performed/1.command-line/managing-application-configuration/config_example/config/prod.exs
@@ -0,0 +1,5 @@
+
+use Mix.Config
+
+config :config_example,
+  message_two: "I'm a production environment message!"
